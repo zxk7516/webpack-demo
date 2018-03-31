a2b537d532215c4606701b180502f3e20871de8c
zxk7516 <zxk7516@foxmail.com>
2018-04-01 06:48:05 +0800

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+import style from './App.css'
+class App extends Component{
+    render(){
+        return <div className="app">
+            <h1>my react app </h1>
+        </div>
+    }
+}
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyjsWebpackPlugin = require("uglifyjs-webpack-plugin");
+const webpack = require('webpack');
+
+module.exports = {
+    entry: { index: './src/index.js', main: './src/admin.js' },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].[hash].js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+            }
+        }, {
+            test: /\.css$/,
+            use: [{
+                loader: 'style-loader'
+            }, {
+                loader: 'css-loader'
+            }
+            ]
+
+        }]
+    },
+    plugins: [
+        new UglifyjsWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+    ]
+}
